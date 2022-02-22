089c0490249e9181166acf4e871a25de771dea7d
Akshatha Dandin <akshathadandin@Abhilashs-MacBook-Pro.local>
2022-02-22 15:05:20 -0800

    [Refactoring] Refactoring tests

--- src/main/java/com/myRetail/product/dto/ItemDetails.java
+++ src/main/java/com/myRetail/product/dto/ItemDetails.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ItemDetails {
-    String tcin;
     ProductDescription product_description;
 }
--- src/test/java/com/myRetail/product/resource/ProductResourceTest.java
+++ src/test/java/com/myRetail/product/resource/ProductResourceTest.java
@@ -3,6 +3,7 @@ package com.myRetail.product.resource;
 import com.myRetail.product.domain.Price;
 import com.myRetail.product.domain.Product;
 import com.myRetail.product.dto.ProductRequestDto;
+import com.myRetail.product.exceptions.ApplicationException;
 import com.myRetail.product.exceptions.BusinessException;
 import com.myRetail.product.exceptions.ProductControllerAdvice;
 import com.myRetail.product.service.ProductService;
@@ -41,7 +42,7 @@ class ProductResourceTest {
     void setUp() {
         mockMvc = MockMvcBuilders
                 .standaloneSetup(productResource)
-                .addPlaceholderValue("service.contextPath", "/api/v1/products")
+                .addPlaceholderValue("service.contextPath", "/product-api/v1/products")
                 .setControllerAdvice(new ProductControllerAdvice())
                 .build();
         ReflectionTestUtils.setField(productResource, "invalidPriceErrorCode",
@@ -60,7 +61,7 @@ class ProductResourceTest {
         Product product = Product.builder().id(id).name("someName").price(
                 Price.builder().value(new BigDecimal("13.45")).currencyCode("USD").build()).build();
         when(productService.getProduct(id)).thenReturn(product);
-        mockMvc.perform(get("/api/v1/products/13860428"))
+        mockMvc.perform(get("/product-api/v1/products/13860428"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().string(
                         "{\"id\":13860428,\"name\":\"someName\",\"" +
@@ -69,7 +70,7 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForInvalidInput() throws Exception {
-        mockMvc.perform(get("/api/v1/products/ssddd"))
+        mockMvc.perform(get("/product-api/v1/products/ssddd"))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("[{\"errorCode\":\"API_ERR_INVALID_PRODUCTID\"" +
                         ",\"errorMessage\":\"Please enter a valid productId\"}]"));
@@ -80,7 +81,7 @@ class ProductResourceTest {
         BigInteger id = new BigInteger("13860428");
         doThrow(new BusinessException("Sorry, requested product is not found", "API_ERR_NOT_FOUND",
                 HttpStatus.NOT_FOUND)).when(productService).getProduct(id);
-        mockMvc.perform(get("/api/v1/products/13860428"))
+        mockMvc.perform(get("/product-api/v1/products/13860428"))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("[{\"errorCode\":\"API_ERR_NOT_FOUND\"" +
                         ",\"errorMessage\":\"Sorry, requested product is not found\"}]"));
@@ -90,7 +91,7 @@ class ProductResourceTest {
     public void shouldBeAbleToUpdateProductDetails() throws Exception {
         BigInteger id = new BigInteger("13860428");
         ProductRequestDto productRequestDto = ProductRequestDto.builder().price("13.45").currency("USD").build();
-        mockMvc.perform(put("/api/v1/products/13860428")
+        mockMvc.perform(put("/product-api/v1/products/13860428")
                         .header("Content-type","application/json")
                         .content("{\"price\":\"14.99\",\"currency\":\"USD\"}"))
                 .andExpect(status().is2xxSuccessful());
@@ -100,7 +101,7 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForInvalidProductIdOnUpdate() throws Exception {
-        mockMvc.perform(put("/api/v1/products/ssddd")
+        mockMvc.perform(put("/product-api/v1/products/ssddd")
                 .header("Content-type","application/json")
                 .content("{\"price\":\"14.99\",\"currency\":\"USD\"}"))
                 .andExpect(status().isBadRequest())
@@ -110,7 +111,7 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForEmptyPriceOnUpdate() throws Exception {
-        mockMvc.perform(put("/api/v1/products/13860428")
+        mockMvc.perform(put("/product-api/v1/products/13860428")
                         .header("Content-type","application/json")
                         .content("{\"currency\":\"USD\"}"))
                 .andExpect(status().isBadRequest())
@@ -120,7 +121,7 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForEmptyCurrencyOnUpdate() throws Exception {
-        mockMvc.perform(put("/api/v1/products/13860428")
+        mockMvc.perform(put("/product-api/v1/products/13860428")
                         .header("Content-type","application/json")
                         .content("{\"price\":\"14.99\"}"))
                 .andExpect(status().isBadRequest())
@@ -130,7 +131,7 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForInvalidInputOnUpdate() throws Exception {
-        mockMvc.perform(put("/api/v1/products/13860428")
+        mockMvc.perform(put("/product-api/v1/products/13860428")
                         .header("Content-type","application/json")
                         .content("{}"))
                 .andExpect(status().isBadRequest())
@@ -139,11 +140,31 @@ class ProductResourceTest {
 
     @Test
     public void shouldThrowBadRequestForInvalidPriceOnUpdate() throws Exception {
-        mockMvc.perform(put("/api/v1/products/13860428")
+        mockMvc.perform(put("/product-api/v1/products/13860428")
                         .header("Content-type","application/json")
                         .content("{\"price\":\"sss\",\"currency\":\"USD\"}"))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("[{\"errorCode\":\"API_ERR_INVALID_PRICE\"," +
                         "\"errorMessage\":\"Please enter a valid price\"}]"));
+    }
+
+    @Test
+    public void shouldHandleApplicationException() throws Exception {
+        BigInteger id = new BigInteger("13860428");
+        doThrow(new ApplicationException("Application exception has occurred")).when(productService).getProduct(id);
+        mockMvc.perform(get("/product-api/v1/products/13860428"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().string("[{\"errorCode\":\"500 INTERNAL_SERVER_ERROR\"," +
+                        "\"errorMessage\":\"Application exception has occurred\"}]"));
+    }
+
+    @Test
+    public void shouldHandleRuntimeException() throws Exception {
+        BigInteger id = new BigInteger("13860428");
+        doThrow(new RuntimeException("Runtime exception has occurred")).when(productService).getProduct(id);
+        mockMvc.perform(get("/product-api/v1/products/13860428"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().string("[{\"errorCode\":\"API_ERR_GENERIC_ERROR\"," +
+                        "\"errorMessage\":\"Oops! An exception has occurred.\"}]"));
     }
 }
